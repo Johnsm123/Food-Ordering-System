94d5af9365f7991a84194cb7fb5fce4a224a8ea4
Johnsm123 <126591412+Johnsm123@users.noreply.github.com>
2025-04-06 14:34:38 +0530

    Create MenuService.java

---
+++ service/MenuService.java
@@ -0,0 +1,10 @@
+package com.examly.service;
+
+import com.examly.entity.MenuItem;
+import com.examly.exception.RestaurantNotFoundException;
+import java.util.List;
+
+public interface MenuService {
+    boolean createMenuItem(MenuItem menuItem) throws RestaurantNotFoundException;
+    List<MenuItem> getMenuItemsByRestaurant(int restaurantId);
+}
