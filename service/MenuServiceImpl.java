06ce5c796392d86e0ed6a20971f3fdf79c9c5213
Johnsm123 <126591412+Johnsm123@users.noreply.github.com>
2025-05-13 15:30:51 +0530

    Update MenuServiceImpl.java

--- service/MenuServiceImpl.java
+++ service/MenuServiceImpl.java
@@ -2,61 +2,66 @@ package com.examly.service;
 
 import com.examly.entity.MenuItem;
 import com.examly.exception.RestaurantNotFoundException;
-import com.examly.util.DbConnectionUtil;
+import com.examly.util.DBConnectionUtil;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-
-public class MenuServiceImpl implements MenuService {
+public class MenuServiceImpl implements MenuService{
     @Override
     public boolean createMenuItem(MenuItem menuItem) throws RestaurantNotFoundException {
         String checkSql = "SELECT COUNT(*) FROM restaurant WHERE restaurantId = ?";
-        String insertSql = "INSERT INTO menu_item (itemId, restaurantId, name, price, description, availableQuantity) VALUES (?, ?, ?, ?, ?, ?)";
+        String insertSql = "INSERT INTO menuItem (restaurantId, name, price, description, availableQuantity) VALUES (?, ?, ?, ?, ?)";
 
-        try (Connection conn = DbConnectionUtil.getConnection()) {
+        try (Connection conn = DBConnectionUtil.getConnection()) {
             PreparedStatement checkStmt = conn.prepareStatement(checkSql);
             checkStmt.setInt(1, menuItem.getRestaurantId());
             ResultSet rs = checkStmt.executeQuery();
-            if (rs.next() && rs.getInt(1) == 0) {
-                throw new RestaurantNotFoundException("Restaurant with ID " + menuItem.getRestaurantId() + " not found");
-            }
+            if(rs.next() && rs.getInt(1) > 0) {
+                PreparedStatement insertStmt = conn.prepareStatement(insertSql);
+                insertStmt.setInt(1, menuItem.getRestaurantId());
+                insertStmt.setString(2, menuItem.getName());
+                insertStmt.setDouble(3, menuItem.getPrice());
+                insertStmt.setString(4, menuItem.getDescription());
+                insertStmt.setInt(5, menuItem.getAvailableQuantity());
+                int rowsAffected = insertStmt.executeUpdate();
+                System.out.println("Menu item insert rows affected: " + rowsAffected);
+                return rowsAffected > 0;
+             }
 
-            PreparedStatement insertStmt = conn.prepareStatement(insertSql);
-            insertStmt.setInt(1, menuItem.getItemId());
-            insertStmt.setInt(2, menuItem.getRestaurantId());
-            insertStmt.setString(3, menuItem.getName());
-            insertStmt.setDouble(4, menuItem.getPrice());
-            insertStmt.setString(5, menuItem.getDescription());
-            insertStmt.setInt(6, menuItem.getAvailableQuantity());
-            return insertStmt.executeUpdate() > 0;
-        } catch (SQLException e) {
+             throw new RestaurantNotFoundException("Restaurant not found: " + menuItem.getRestaurantId());
+           
+        }catch(SQLException e) {
+            System.err.println("SQL Error in createMenuItem: " + e.getMessage());
             e.printStackTrace();
             return false;
         }
     }
-
     @Override
     public List<MenuItem> getMenuItemsByRestaurant(int restaurantId) {
         List<MenuItem> menuItems = new ArrayList<>();
-        String sql = "SELECT * FROM menu_item WHERE restaurantId = ?";
-        try (Connection conn = DbConnectionUtil.getConnection();
+        String sql = "SELECT * FROM menuItem WHERE restaurantId = ?";
+        try (Connection conn = DBConnectionUtil.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
-            stmt.setInt(1, restaurantId);
-            ResultSet rs = stmt.executeQuery();
-            while (rs.next()) {
-                MenuItem item = new MenuItem(
+             stmt.setInt(1, restaurantId);
+             ResultSet rs = stmt.executeQuery();
+             while (rs.next()){
+                menuItems.add ( new MenuItem(
                     rs.getInt("itemId"),
                     rs.getInt("restaurantId"),
                     rs.getString("name"),
                     rs.getDouble("price"),
                     rs.getString("description"),
                     rs.getInt("availableQuantity")
-                );
-                menuItems.add(item);
+                ));
+                
+             }
+
+            }catch (SQLException e){
+                e.printStackTrace();
+
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return menuItems;
+            return menuItems;
     }
+    
+   
 }
