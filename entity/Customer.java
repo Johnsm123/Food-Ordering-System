9ed6230baff39297555b676ca4baabaa0cd94fcc
Johnsm123 <126591412+Johnsm123@users.noreply.github.com>
2025-05-13 15:32:38 +0530

    Update Customer.java

--- entity/Customer.java
+++ entity/Customer.java
@@ -9,23 +9,24 @@ public class Customer {
 
     public Customer() {}
 
-    public Customer(int customerId, String name, String email, String phoneNumber, String password) {
+    public Customer(int customerId, String name, String email, String phoneNumber, String password){
         this.customerId = customerId;
         this.name = name;
         this.email = email;
         this.phoneNumber = phoneNumber;
         this.password = password;
     }
+    
 
-    // Getters and Setters
     public int getCustomerId() { return customerId; }
-    public void setCustomerId(int customerId) { this.customerId = customerId; }
+    public void setCustomerId(int customerId) { this.customerId = customerId;}
     public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public void setName(String name) { this.name = name;}
+    public String getEmail() { return email;}
+    public void setEmail(String email){ this.email = email;}
     public String getPhoneNumber() { return phoneNumber; }
-    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public void setPhoneNumber(String phoneNumber){ this.phoneNumber = phoneNumber; }
+    public String getPassword() { return password;}
+    public void setPassword(String password){ this.password = password;}
 }
+
